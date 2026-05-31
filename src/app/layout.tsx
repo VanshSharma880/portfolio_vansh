af2ea72268d9321fa4d93c1504e5c3796309ce00
Vansh Sharma <svansh880@gmail.com>
2026-05-31 21:21:42 +0530

    chore: update dependencies and improve TypeScript configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <link rel="icon" href="/favicon.ico" type="image/x-icon" />
       <body className={montserrat.className}>
         {" "}
